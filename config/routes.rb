95362e4e7ea41abc7a44a66a4b8295333ca4dde4
imubarak234 <imubarak2424@gmail.com>
2022-07-09 14:32:41 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/home', to: 'home#index'
+
+  resources : :users, only: [:index, :new, :create, :show]
+  resources : :members, only: [:index, :new, :create, :show]
+  resources : :groups, only: [:index, :new, :create, :show]
 end
