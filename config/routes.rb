43eaf5d2dbbaf948473c9ad72666e268203606ba
imubarak234 <imubarak2424@gmail.com>
2022-07-09 14:22:37 +0100

    home setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/home', to: 'home#index'
 end
